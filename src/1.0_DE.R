6c195d2e958e1cb2ca0c57eaa146813c2f88eb2b
Zhilong Jia <zhilongjia@gmail.com>
2020-01-30 12:01:02 +0800

    add protein-coding or lncRNA infor.

--- src/1.0_DE.R
+++ src/1.0_DE.R
@@ -12,6 +12,9 @@ rownames(S_raw) <- sample_count_df_symbol$SYMBOL
 dge <- DGEList(counts=S_raw)
 
 ################################################################################
+mRNA_lncRNA <- readr::read_tsv("../data/ensembl_symbol_type.tsv",col_names = FALSE)
+colnames(mRNA_lncRNA) <- c("ensembl", "symbol", "type")
+################################################################################
 comp2groups <- list(nCoV_Heal=c("Healthy", "nCoV"), 
                     pneu_Heal=c("Healthy", "pneumonia"), 
                     nCoV_pneu=c("pneumonia", "nCoV") )
@@ -41,9 +44,13 @@ for (comp_var in names(comp2groups) ) {
     Expfit1 <- lmFit(v, Expdesign)
     Expfit2 <- eBayes(Expfit1)
     limma_res <- topTable(Expfit2, coef=tail(colnames(Expdesign), 1), number=Inf) %>% 
-        tibble::rownames_to_column(var=comp_var )
+        tibble::rownames_to_column() %>% 
+        dplyr::left_join(mRNA_lncRNA, by=c("rowname"="symbol")) 
+    colnames(limma_res)[1] <- comp_var
+
     
-    limma_DEA <- dplyr::filter(limma_res, adj.P.Val<=0.05, abs(logFC)>=2 )
+    # only protein_coding genes
+    limma_DEA <- dplyr::filter(limma_res, adj.P.Val<=0.05, abs(logFC)>=2, type=="protein_coding" )
     readr::write_tsv(limma_DEA, paste0("../results/", comp_var, "_limma_res.tsv") )
     
     limma_UP <- dplyr::filter(limma_DEA, logFC>0)[[comp_var]]
@@ -53,7 +60,7 @@ for (comp_var in names(comp2groups) ) {
     print(length(limma_DEG))
     limma_DE <- v$E[limma_DEG,]
     
-    
+    # limma_res include lncRNA, other objs exclude lncRNA
     DEA_list[["limma_res"]][[comp_var]] <- limma_res
     DEA_list[["limma_DEA"]][[comp_var]] <- limma_DEA
     DEA_list[["limma_DEG"]][[comp_var]] <- limma_DEG
@@ -73,6 +80,9 @@ autoplot(prcomp(t(DEA_list[["limma_DE"]][["nCoV_pneu"]]), scale=F),
          size = 8, label = TRUE, label.colour="black", ts.colour="black" )
 
 library(venn)
+
+venn(DEA_list[["limma_DEG"]], ilab=TRUE, zcolor = "style", cexil=10, cexsn=10)
+
 venn(DEA_list[["limma_UPDN"]][c("nCoV_Heal_UP", "nCoV_Heal_DN", "pneu_Heal_UP", "pneu_Heal_DN")], ilab=TRUE, zcolor = "style", cexil=10, cexsn=10)
 
 save(S_raw, DEA_list, sample_meta, file="../results/DEA_list.RData")
