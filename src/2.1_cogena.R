4eae79892ae4345803bec3f139e44ebd1e6eb3ec
Zhilong Jia <zhilongjia@gmail.com>
2020-02-02 02:48:11 +0800

    clean cogena-run code to save image.

--- src/2.1_cogena.R
+++ src/2.1_cogena.R
@@ -6,11 +6,11 @@ nCoV_Heal_DE <- DEA_list[["limma_DE"]][["nCoV_Heal"]]
 nCoV_Heal_pheno <- DEA_list[["subsample_pheno"]][["nCoV_Heal"]] 
 
 
-sampleLabel <- nCoV_Heal_pheno$Group
+sampleLabel <- nCoV_Heal_pheno$Group1
 names(sampleLabel) <- nCoV_Heal_pheno$sampleID
 ################################################################################
-# devtools::load_all("~/data/cogena")
-library(cogena)
+devtools::load_all("../../cogena")
+# library(cogena)
 
 nClust <- 2:10
 ncore <- 7
@@ -28,9 +28,6 @@ genecl_result <- coExp(nCoV_Heal_DE, nClust=nClust,
 ###########################################
 ##########################################
 annoGMT <- "c2.cp.kegg.v7.0.symbols.gmt"
-# annoGMT <- "c5.all.v6.1.symbols.gmt"; nClust =3; clMethods=c("kmeans")
-annoGMT <- "CmapDn100.gmt.xz"; nClust =5; clMethods=c("kmeans")
-annoGMT <- "CmapUp100.gmt.xz"; nClust =3; clMethods=c("kmeans")
 
 
 annofile <- system.file("extdata", annoGMT, package="cogena")
@@ -40,21 +37,44 @@ cogena_result <- clEnrich(genecl_result, annofile=annofile, sampleLabel=sampleLa
 
 summary(cogena_result)
 
-heatmapCluster(cogena_result, "kmeans", "7", maintitle="nCoV_Heal", add2=FALSE,
+heatmapCluster(cogena_result, "h", "3", maintitle="nCoV_Heal", add2=FALSE,
                cexCol=1.2 )
 
-heatmapPEI(cogena_result, "k", "3", maintitle="nCoV_Heal", add2=FALSE,
+heatmapPEI(cogena_result, "h", "3", maintitle="nCoV_Heal", add2=FALSE,
            CutoffNumGeneset=20)
 
-heatmapPEI(cogena_result, "k", "3", maintitle="nCoV_Heal", add2=FALSE,
-           CutoffNumGeneset=20, orderMethod = "1", printGS = TRUE)
-heatmapPEI(cogena_result, "k", "5", maintitle="nCoV_Heal", add2=FALSE,
-           CutoffNumGeneset=20, orderMethod = "5", printGS = TRUE)
-
-
-heatmapPEI(cogena_result, "k", "5", maintitle="nCoV_Heal", add2=FALSE,
-           CutoffNumGeneset=20, orderMethod = "2", printGS = TRUE)
 
 save.image("../results/2.1_cogena_KEGG.RData")
+
+#########################################################################
+annoGMT <- "CmapUp100.gmt.xz";
+annofile <- system.file("extdata", annoGMT, package="cogena")
+drugUP_result <- clEnrich(genecl_result, annofile=annofile, sampleLabel=sampleLabel, ncore=ncore)
+
+heatmapPEI(drugUP_result, "h", "3", maintitle="nCoV_Heal", add2=TRUE,
+           CutoffNumGeneset=20, orderMethod = "2", printGS = TRUE)
+
+heatmapPEI(drugUP_result, "h", "3", maintitle="nCoV_Heal", add2=TRUE,
+           CutoffNumGeneset=20, orderMethod = "3", printGS = TRUE)
+
+heatmapPEI(drugUP_result, "h", "3", maintitle="nCoV_Heal", add2=TRUE,
+           CutoffNumGeneset=20, orderMethod = "Down", printGS = TRUE)
+
 save.image("../results/2.1_cogena_CmapUP.RData")
+
+
+#########################################################################
+annoGMT <- "CmapDn100.gmt.xz"; 
+annofile <- system.file("extdata", annoGMT, package="cogena")
+drugDN_result <- clEnrich(genecl_result, annofile=annofile, sampleLabel=sampleLabel, ncore=ncore)
+
+heatmapPEI(drugDN_result, "h", "3", maintitle="nCoV_Heal", add2=TRUE,
+           CutoffNumGeneset=20, orderMethod = "1", printGS = TRUE)
+
+heatmapPEI(drugDN_result, "h", "3", maintitle="nCoV_Heal", add2=TRUE,
+           CutoffNumGeneset=20, orderMethod = "Up", printGS = TRUE)
+
 save.image("../results/2.1_cogena_CmapDN.RData")
+
+#########################################################################
+
