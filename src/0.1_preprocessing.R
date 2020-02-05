c4d456dbf1685159dcdc0cf556725fd1a031a557
Zhilong Jia <zhilongjia@gmail.com>
2020-02-05 20:26:54 +0800

    add slides.

--- src/0.1_preprocessing.R
+++ src/0.1_preprocessing.R
@@ -1,17 +1,33 @@
 
+
+# sample_meta <- readr::read_csv("../data/sample_meta-v1.1.csv") # ALL
+# sample_meta <- readr::read_csv("../data/sample_meta-v2.csv") # SE
+# sample_meta <- readr::read_csv("../data/sample_meta-v3.csv") # PE
+sample_meta <- readxl::read_xlsx("../data/sample_meta_v8.xlsx") # SE
+
+sampID_newID <- sample_meta$NewID
+names(sampID_newID) <- sample_meta$sampleID
+
+
+# RNA-Seq data
 library(dplyr)
 gene_count_rpkm_fn <- dir("../data/final/", pattern="Counts", full.names=TRUE)
 
 gene_count_list <- list()
 for (fn_i in gene_count_rpkm_fn ) {
+    # fn_i <- gene_count_rpkm_fn[1]
     gene_count_rpkm <- readr::read_tsv(fn_i) %>% 
         dplyr::group_by(Geneid) %>%
         summarise_all(funs(sum))
     
     sample_id <- sub("_Counts.txt","",basename(fn_i) )
-    colnames(gene_count_rpkm)[2] <- sample_id
     
-    gene_count_list[[sample_id]] <- gene_count_rpkm
+    #  use NewID as sample names
+    if (!is.na(sampID_newID[sample_id]) ) {
+        colnames(gene_count_rpkm)[2] <- sampID_newID[sample_id]
+        gene_count_list[[sample_id]] <- gene_count_rpkm
+    }
+    
 }
 
 sample_count_df <- Reduce(dplyr::inner_join, gene_count_list )
@@ -19,35 +35,35 @@ sample_count_df <- Reduce(dplyr::inner_join, gene_count_list )
 
 # combine PE and SE samples
 # ref: https://www.biostars.org/p/252552/
-sample_count_df$`x19001-R` <- sample_count_df$`x19001-R` + sample_count_df$`Paried_x19001-R`
-sample_count_df$`x19002-R` <- sample_count_df$`x19002-R` + sample_count_df$`Paried_x19002-R`
-sample_count_df$`x19003-R` <- sample_count_df$`x19003-R` + sample_count_df$`Paried_x19003-R`
-sample_count_df$`x19004-R` <- sample_count_df$`x19004-R` + sample_count_df$`Paried_x19004-R`
+sample_count_df$C1 <- sample_count_df$C1_1 + sample_count_df$C1
+sample_count_df$C2 <- sample_count_df$C2_1 + sample_count_df$C2
+sample_count_df$C3 <- sample_count_df$C3_1 + sample_count_df$C3
+sample_count_df$C4 <- sample_count_df$C4_1 + sample_count_df$C4
+
+sample_count_df <- dplyr::select(sample_count_df, -one_of(c("C1_1", "C2_1", "C3_1", "C4_1")) )
 
 
 readr::write_tsv(sample_count_df, "../results/all_genecounts.tsv")
 
-# sample_meta <- readr::read_csv("../data/sample_meta-v1.1.csv") # ALL
-sample_meta <- readr::read_csv("../data/sample_meta-v2.csv") # SE
-# sample_meta <- readr::read_csv("../data/sample_meta-v3.csv") # PE
+################################################################################
 
 
 save.image("../results/0.1_preprocessing_stage1.RData")
 
 ################################################################################
 # sample grouped
-nCoV_samp <- dplyr::filter(sample_meta, Group1=="nCoV")  %>% 
-    dplyr::select(sampleID) %>% unlist(use.names = FALSE)  
-Heal_samp <- dplyr::filter(sample_meta, Group1=="Healthy")  %>% 
-    dplyr::select(sampleID) %>% unlist(use.names = FALSE)  
-pneu_samp <- dplyr::filter(sample_meta, Group1=="pneumonia")  %>% 
-    dplyr::select(sampleID) %>% unlist(use.names = FALSE)  
+nCoV_samp <- dplyr::filter(sample_meta, Diagnostics=="nCoV pneumonia")  %>% 
+    dplyr::select(NewID) %>% unlist(use.names = FALSE)  
+Heal_samp <- dplyr::filter(sample_meta, Diagnostics=="Healthy")  %>% 
+    dplyr::select(NewID) %>% unlist(use.names = FALSE)  
+pneu_samp <- dplyr::filter(sample_meta, Diagnostics=="CAP")  %>% 
+    dplyr::select(NewID) %>% unlist(use.names = FALSE)  
 
-pneuVir_samp <- dplyr::filter(sample_meta, Group3=="pneumonia-vir")  %>% 
-    dplyr::select(sampleID) %>% unlist(use.names = FALSE)  
+pneuVir_samp <- dplyr::filter(sample_meta, Types=="Viral-like")  %>% 
+    dplyr::select(NewID) %>% unlist(use.names = FALSE)  
 
-pneuBac_samp <- dplyr::filter(sample_meta, Group3=="pneumonia-bac")  %>% 
-    dplyr::select(sampleID) %>% unlist(use.names = FALSE)  
+pneuBac_samp <- dplyr::filter(sample_meta, Types=="Others")  %>% 
+    dplyr::select(NewID) %>% unlist(use.names = FALSE)  
 
 
 ################################################################################
@@ -61,18 +77,25 @@ symbol_ENTREZID <- clusterProfiler::bitr(gsub("\\..+", "", sample_count_df$Genei
                                          fromType="ENSEMBL", toType=c("ENSEMBL", "SYMBOL"), OrgDb="org.Hs.eg.db", drop=TRUE)
 
 # get symbol gene expression
-sample_count_df_symbol <- dplyr::select(sample_count_df, c("Geneid", all_of(c(nCoV_samp, Heal_samp, pneu_samp)))) %>% 
-    # dplyr::filter(Geneid %in% nlq_ensembl) %>% 
-    dplyr::mutate(ENSEMBL=gsub("\\..+", "", Geneid )) %>% 
+sample_count_df_symbol <- dplyr::mutate(sample_count_df, ENSEMBL=gsub("\\..+", "", Geneid )) %>% 
     dplyr::left_join(symbol_ENTREZID ) %>% 
     dplyr::filter(!is.na(SYMBOL) ) %>% 
     dplyr::select("SYMBOL",nCoV_samp, Heal_samp, pneu_samp) %>%
     dplyr::group_by(SYMBOL) %>% 
     dplyr::summarise_all(sum)
 
+
+
+sample_meta <- dplyr::filter(sample_meta, Diagnostics %in% c("nCoV pneumonia",
+                                                             "Healthy",
+                                                             "CAP") )
+
+
 save.image("../results/0.1_preprocessing.RData")
 
 
-save(sample_count_df_symbol, sample_meta, file="../results/sample_count_df_symbol.RData")
+save(sample_count_df_symbol, sample_meta, 
+     nCoV_samp, Heal_samp, pneu_samp, pneuVir_samp, pneuBac_samp,
+     file="../results/sample_count_df_symbol.RData")
 
 
--- src/1.2_DE_pneu.R
+++ src/1.2_DE_pneu.R
@@ -11,18 +11,26 @@ S_raw <- as.matrix(dplyr::select(sample_count_df_symbol, -SYMBOL) )
 rownames(S_raw) <- sample_count_df_symbol$SYMBOL
 dge <- DGEList(counts=S_raw)
 
-sample_meta <- readr::read_csv("../data/sample_meta-v2.csv")
+# sample_meta <- readr::read_csv("../data/sample_meta-v2.csv")
 
 ################################################################################
 mRNA_lncRNA <- readr::read_tsv("../data/ensembl_symbol_type.tsv",col_names = FALSE)
 colnames(mRNA_lncRNA) <- c("ensembl", "symbol", "type")
 mRNA_lncRNA <- unique(mRNA_lncRNA[,-1])
 ################################################################################
-comp2groups <- list(pneuVir_Heal=c("Healthy", "pneumonia-vir"), 
-                    pneuBac_Heal=c("Healthy", "pneumonia-bca"), 
-                    pneuBac_pneuVir=c("pneumonia-bca", "pneumonia-vir"),
-                    nCoV_pneuBac=c("pneumonia-bca", "nCoV"),
-                    nCoV_pneuVir=c("pneumonia-vir", "nCoV") )
+comp2groups <- list(nCoV_Heal=c("Healthy", "nCoV"),
+                    Vir_Heal=c("Healthy", "Viral-like"), 
+                    Others_Heal=c("Healthy", "Others"), 
+                    # pneuBac_pneuVir=c("Others", "Viral-like"),
+                    # nCoV_pneuBac=c("Others", "nCoV"),
+                    nCoV_Vir=c("Viral-like", "nCoV") )
+
+# comp2groups <- list(pneuVir_Heal=c("Healthy", "pneumonia-vir"), 
+#                     pneuBac_Heal=c("Healthy", "pneumonia-bca"), 
+#                     pneuBac_pneuVir=c("pneumonia-bca", "pneumonia-vir"),
+#                     nCoV_pneuBac=c("pneumonia-bca", "nCoV"),
+#                     nCoV_pneuVir=c("pneumonia-vir", "nCoV") )
+# 
 
 DEA_pneu_list <- list()
 
@@ -30,11 +38,11 @@ for (comp_var in names(comp2groups) ) {
     # comp_var <- names(comp2groups)[[1]]
     comp_i <- comp2groups[[comp_var]]
     print (comp_i)
-    subsample_pheno <- dplyr::filter(sample_meta, Group3 %in% comp_i )
-    subsample_pheno$Group3 <- factor(subsample_pheno$Group3, levels=comp_i )
-    Expdesign <- model.matrix(~subsample_pheno$Group3)
+    subsample_pheno <- dplyr::filter(sample_meta, Types %in% comp_i )
+    subsample_pheno$Types <- factor(subsample_pheno$Types, levels=comp_i )
+    Expdesign <- model.matrix(~subsample_pheno$Types)
     
-    subsample_dge <- dge[,subsample_pheno$sampleID]
+    subsample_dge <- dge[,subsample_pheno$NewID]
     
     # DEA
     keep <- filterByExpr(subsample_dge, Expdesign)
@@ -56,7 +64,7 @@ for (comp_var in names(comp2groups) ) {
     
     # only protein_coding genes
     limma_DEA <- dplyr::filter(limma_res, adj.P.Val<=0.05, abs(logFC)>=2, type=="protein_coding" )
-    readr::write_tsv(limma_DEA, paste0("../results/", comp_var, "_limma_res.tsv") )
+    readr::write_tsv(limma_DEA, paste0("../results/limma/", comp_var, "_limma_res.tsv") )
     
     limma_UP <- dplyr::filter(limma_DEA, logFC>0)[[comp_var]]
     limma_DN <- dplyr::filter(limma_DEA, logFC<0)[[comp_var]]
@@ -79,40 +87,44 @@ for (comp_var in names(comp2groups) ) {
 save.image("../results/1.0_DE_pneu.RData")
 
 ################################################################################
-library(ggfortify)
-autoplot(prcomp(t(DEA_pneu_list[["limma_DE"]][["pneuBac_pneuVir"]]), scale=F), 
-         data=DEA_pneu_list[["subsample_pheno"]][["pneuBac_pneuVir"]], colour = "Group3", 
-         size = 8, label = F, label.colour="black", ts.colour="black" )
-
-autoplot(prcomp(t(DEA_pneu_list[["limma_DE"]][["nCoV_pneuBac"]]), scale=F), 
-         data=DEA_pneu_list[["subsample_pheno"]][["nCoV_pneuBac"]], colour = "Group3", 
-         size = 8, label = TRUE, label.colour="black", ts.colour="black" )
-
-autoplot(prcomp(t(DEA_pneu_list[["limma_DE"]][["nCoV_pneuVir"]]), scale=F), 
-         data=DEA_pneu_list[["subsample_pheno"]][["nCoV_pneuVir"]], colour = "Group3", 
-         size = 8, label = TRUE, label.colour="black", ts.colour="black" )
+# library(ggfortify)
+# autoplot(prcomp(t(DEA_pneu_list[["limma_DE"]][["pneuBac_pneuVir"]]), scale=F), 
+#          data=DEA_pneu_list[["subsample_pheno"]][["pneuBac_pneuVir"]], colour = "Types", 
+#          size = 8, label = F, label.colour="black", ts.colour="black" )
+# 
+# autoplot(prcomp(t(DEA_pneu_list[["limma_DE"]][["nCoV_pneuBac"]]), scale=F), 
+#          data=DEA_pneu_list[["subsample_pheno"]][["nCoV_pneuBac"]], colour = "Types", 
+#          size = 8, label = TRUE, label.colour="black", ts.colour="black" )
+# 
+# autoplot(prcomp(t(DEA_pneu_list[["limma_DE"]][["nCoV_pneuVir"]]), scale=F), 
+#          data=DEA_pneu_list[["subsample_pheno"]][["nCoV_pneuVir"]], colour = "Types", 
+#          size = 8, label = TRUE, label.colour="black", ts.colour="black" )
 
 
 
 library(venn)
 
-venn::venn(DEA_pneu_list[["limma_DEG"]], ilab=TRUE, zcolor = "style", 
+# venn::venn(DEA_pneu_list[["limma_DEG"]], ilab=TRUE, zcolor = "style", 
+#            box=FALSE, ilcs=1.5, sncs=1.1, cexil=10, cexsn=10)
+venn::venn(DEA_pneu_list[["limma_DEG"]][c("nCoV_Heal","Vir_Heal","Others_Heal")], 
+           ilab=TRUE, zcolor = "style", 
            box=FALSE, ilcs=1.5, sncs=1.2, cexil=10, cexsn=10)
 
-tmp <- c("pneuVir_Heal","pneuBac_Heal")
-
-venn::venn(DEA_pneu_list[["limma_DEG"]][c("pneuVir_Heal","pneuBac_Heal")], ilab=TRUE, zcolor = "style", 
-           box=FALSE, ilcs=1.5, sncs=1.2, cexil=10, cexsn=10)
-
-
-venn::venn(DEA_pneu_list[["limma_UPDN"]][c("pneuVir_Heal_UP", "pneuVir_Heal_DN", 
-                                      "pneuBac_Heal_UP", "pneuBac_Heal_DN")], 
-           box=FALSE, ilcs=1.5, sncs=1.5, ilab=TRUE, zcolor = "style", cexil=5, cexsn=10)
-
-venn::venn(DEA_pneu_list[["limma_UPDN"]][c("nCoV_pneuVir_UP", "nCoV_pneuVir_DN", 
-                                      "nCoV_pneuBac_UP", "nCoV_pneuBac_DN")], 
-           box=FALSE, ilcs=1.5, sncs=1.5, ilab=TRUE, zcolor = "style", cexil=5, cexsn=10)
-
-save(S_raw, DEA_pneu_list, sample_meta, file="../results/DEA_pneu_list.RData")
+venn::venn(DEA_pneu_list[["limma_UPDN"]][c("nCoV_Heal_UP", "nCoV_Heal_DN", 
+                                      "Vir_Heal_UP", "Vir_Heal_DN")], 
+           box=FALSE, ilcs=1.5, sncs=1.2, ilab=TRUE, zcolor = "style", cexil=5, cexsn=10)
 
 
+# venn::venn(DEA_pneu_list[["limma_UPDN"]][c("nCoV_pneuVir_UP", "nCoV_pneuVir_DN", 
+#                                       "nCoV_pneuBac_UP", "nCoV_pneuBac_DN")], 
+#            box=FALSE, ilcs=1.5, sncs=1.5, ilab=TRUE, zcolor = "style", cexil=5, cexsn=10)
+
+
+# output veen res
+venn_res <- gplots::venn(DEA_pneu_list[["limma_DEG"]][c("nCoV_Heal","Vir_Heal","Others_Heal")])
+aa <- attr(venn_res,"intersections")
+aa_df <- as.data.frame(sapply(aa, "length<-", max(lengths(aa))))
+readr::write_csv(aa_df, path="../results/venn/DEG_venn_res.csv", na="")
+
+DEA_list <- DEA_pneu_list
+save(S_raw, DEA_list, sample_meta, file="../results/DEA_pneu_list.RData")
